5eb44ed6808f87c19e02c3bd8ff1f4a7a45d099f
Can Surer <cansurer@outlook.com>
2023-02-05 21:32:51 +0100

    merge

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,13 @@
 /// <reference types="@types/google.maps" />
+
 import { User } from "./User";
 import { Company } from "./Company";
+import { CustomMap } from "./CustomMap";
 
 const user = new User();
-
-console.log(user);
-
 const company = new Company();
-console.log(company);
+const customMap = new CustomMap("map");
 
+customMap.addMaker(user);
+customMap.addMaker(company);
 
